212a97c48b49f16c3ab9eb3815078f6b5c0d1d2b
marcin <marcin@vaadin.com>
2025-12-01 19:00:01 +0100

    simplify

--- day01.py
+++ day01.py
@@ -18,31 +18,27 @@ answer2 = 6
 dial_size = 100 # 0 .. 99
 start = 50
 
-# Replace L with '-', remove R
+# Convert input to array of ints
 def prepareData(data):
-    return str(data).replace('L', '-').replace('R', '').splitlines()
+    return map(int, str(data).replace('L', '-').replace('R', '').splitlines())
 
 # Task - count when dial points 0
 # dial size = 0 .. 99 => 100
 # points 0 when sum % 100 = 0
-def solution01(lines):
+def solution01(data):
     total = 0
     current = start
-    data = prepareData(lines)
-    for d in data:
-        n = int(d)
+    for n in data:
         current += n
         if (current % dial_size == 0):
             total += 1
     return total
 
 # Task - count how many times dial passed 0
-def solution02(lines):
+def solution02(data):
     total = 0
     current = start
-    data = prepareData(lines)
-    for d in data:
-        n = int(d)
+    for n in data:
         prev = current
         current += n
         for i in range(prev, current, 1 if n > 0 else -1):
@@ -51,12 +47,12 @@ def solution02(lines):
     return total
 
 # Test example data
-assert(answer1 == solution01(example1))
-assert(answer2 == solution02(example1))  
+assert(answer1 == solution01(prepareData(example1)))
+assert(answer2 == solution02(prepareData(example1)))  
 
 # Load and check real data
 with open('input01.txt') as f:
       lines = f.read()
 
-print("Solution 01: " + str(solution01(lines)))
-print("Solution 02: " + str(solution02(lines)))
+print("Solution 01: " + str(solution01(prepareData(lines))))
+print("Solution 02: " + str(solution02(prepareData(lines))))
