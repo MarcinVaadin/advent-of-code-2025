971d5766ec4a60221e3bc74a800ad14db10825d4
marcin <marcin@vaadin.com>
2025-12-01 18:28:40 +0100

    Day 01

---
+++ day01.py
@@ -0,0 +1,62 @@
+
+# Example values and answer
+example1 = """L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+"""
+answer1 = 3
+answer2 = 6
+
+# Parameters
+dial_size = 100 # 0 .. 99
+start = 50
+
+# Replace L with '-', remove R
+def prepareData(data):
+    return str(data).replace('L', '-').replace('R', '').splitlines()
+
+# Task - count when dial points 0
+# dial size = 0 .. 99 => 100
+# points 0 when sum % 100 = 0
+def solution01(lines):
+    total = 0
+    current = start
+    data = prepareData(lines)
+    for d in data:
+        n = int(d)
+        current += n
+        if (current % dial_size == 0):
+            total += 1
+    return total
+
+# Task - count how many times dial passed 0
+def solution02(lines):
+    total = 0
+    current = start
+    data = prepareData(lines)
+    for d in data:
+        n = int(d)
+        prev = current
+        current += n
+        for i in range(prev, current, 1 if n > 0 else -1):
+            if (i % dial_size == 0):
+                total += 1
+    return total
+
+# Test example data
+assert(answer1 == solution01(example1))
+assert(answer2 == solution02(example1))  
+
+# Load and check real data
+with open('input01.txt') as f:
+      lines = f.read()
+
+print("Solution 01: " + str(solution01(lines)))
+print("Solution 02: " + str(solution02(lines)))
